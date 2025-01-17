bec9b4f337c3d4fb3d72eb909a52791812215d6f
YoungJaeIm <wpem2014@naver.com>
2025-01-17 19:41:47 +0900

    feature: 로그인 기능 및 user db 추가

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.User = require("./user")(sequelize, DataTypes);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Database synced");
+  })
+  .catch((error) => {
+    console.error("Error syncing database:", error);
+  });
+
+module.exports = db;
---
+++ passport/naverStrategy.js
@@ -0,0 +1,32 @@
+const NaverStrategy = require("passport-naver").Strategy;
+const dotenv = require("dotenv");
+const { findOrCreateUser } = require("../controllers/authController");
+
+dotenv.config({ path: "./src/config/.env" });
+
+const naverStrategy = new NaverStrategy(
+  {
+    clientID: process.env.NAVER_CLIENT_ID,
+    clientSecret: process.env.NAVER_CLIENT_SECRET,
+    callbackURL: process.env.NAVER_REDIRECT_URL,
+  },
+  async (accessToken, refreshToken, profile, cb) => {
+    const email = profile._json.email;
+    const provider = profile.provider;
+    const phoneNumber = profile.phone_number;
+    const name = profile.displayName;
+
+    if (!email) {
+      return cb(new Error("No email found in profile"));
+    }
+
+    const user = await findOrCreateUser(email, provider, phoneNumber, name);
+    if (!user) {
+      return cb(null, false);
+    }
+
+    return cb(null, user);
+  },
+);
+
+module.exports = naverStrategy;
