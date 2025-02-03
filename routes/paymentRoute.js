552b0eb8b9ca03135c6f491bb2e22820a1c4da74
inyoung <ekseh789@gmail.com>
2025-02-03 15:19:53 +0900

    Update paymentRoute.js

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const { 
-    completePayment,
-    postPayment,
-    //refundPayment
- } = require("../controllers/paymentsController");
+const {
+  completePayment,
+  postPayment,
+  refundPayment,
+} = require("../controllers/paymentController");
 const router = express.Router();
 const { verifyToken } = require("../middlewares/jwtMiddleware");
 
 router.use(express.json());
 
-router.post("/complete", verifyToken, completePayment);
-router.post("/:paymentId", verifyToken, postPayment);
-//router.delete("/refund", refundPayment);
-
-module.exports = router;
+module.exports = (io) => {
+  router.post("/complete", verifyToken, completePayment);
+  router.post("/:paymentId", verifyToken, postPayment);
+  router.post("/refund", verifyToken, (req, res) =>
+    refundPayment(req, res, io)
+  );
+  return router;
+};
